3cd26b94f5549c260660fdfd8f4a6db34273c9d9
winlawson <nlawson3@illinois.edu>
2023-04-11 22:33:30 -0500

    current state of data analysis, fitting function is not working, but otherwise data is complete

--- analysis.py
+++ analysis.py
@@ -1,21 +1,22 @@
-import math
-
 import numpy as np
 import pandas
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
-data = pandas.read_csv('data/weekend-3-23.csv',
-                       usecols=['STDC_8_0', 'STDC_8_1', 'STDC_8_2', 'STDC_8_3', 'STDC_8_4', 'STDC_8_5', 'STDC_8_6',
-                                'STDC_8_7', 'STDC_8_8', 'STDC_8_SUM'])
-data2 = pandas.read_csv('data/weekend-3-23.csv',
+data1 = pandas.read_csv('data/3-23-23-CUplates-magnet.csv',
+                        usecols=['STDC_8_0', 'STDC_8_1', 'STDC_8_2', 'STDC_8_3', 'STDC_8_4', 'STDC_8_5', 'STDC_8_6',
+                                 'STDC_8_7', 'STDC_8_8', 'STDC_8_SUM'])
+data2 = pandas.read_csv('data/3-30-23-CUplates-magnet.csv',
                         usecols=['STDC_8_0', 'STDC_8_1', 'STDC_8_2', 'STDC_8_3', 'STDC_8_4', 'STDC_8_5', 'STDC_8_6',
                                  'STDC_8_7', 'STDC_8_15', 'STDC_8_SUM'])
 data2.rename(columns={'STDC_8_15': 'STDC_8_8'}, inplace=True)
-data = pandas.concat([data, data2])
+data3 = pandas.read_csv('data/4-4-23-CUplates-magnet.csv',
+                        usecols=['STDC_8_0', 'STDC_8_1', 'STDC_8_2', 'STDC_8_3', 'STDC_8_4', 'STDC_8_5', 'STDC_8_6',
+                                 'STDC_8_7', 'STDC_8_15', 'STDC_8_SUM'])
+data3.rename(columns={'STDC_8_15': 'STDC_8_8'}, inplace=True)
+data = pandas.concat([data1, data2, data3])
 data = data[data['STDC_8_SUM'] > 1]
 
-up_counts = 0
-down_counts = 0
 up_list = []
 down_list = []
 for index, row in data.iterrows():
@@ -24,27 +25,36 @@ for index, row in data.iterrows():
     row = row[row != 4095]
     if row.argmax() < row.argmin():
         up_list.append(row.min())
-        up_counts += 1
     elif row.argmax() > row.argmin():
         down_list.append(row.min())
-        down_counts += 1
 
-print(up_counts)
-print(down_counts)
+n_bins = 20
+n_up, bin_edges = np.histogram(up_list, n_bins)  # Make a histogram
+n_down, bin_edges = np.histogram(down_list, n_bins)
 
-nbins = 35
-n_up, bin_edges = np.histogram(up_list, nbins)  # Make a histogram
-n_down, bin_edges = np.histogram(down_list, nbins)
+up_minus_down = n_up - n_down
 
-the_thing_to_plot = n_up - n_down
+
+def damped_sine(x, b, c, d, e):
+    return b * np.exp(-x/c) * np.sin(d * x + e)
+
+
+bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+bin_centers *= (8 / 4096)
+popt, pcov = curve_fit(damped_sine, bin_centers, up_minus_down)
+errs = np.sqrt(n_down ^ 2 + n_up ^ 2)
+
+"""
+plt.hist(up_minus_down, n_bins, density=True, alpha=0.5)
+plt.plot(bin_centers,damped_sine((bin_centers, *popt), 'r-', label='fit'))
+plt.legend()
+plt.show()"""
 
 fig, ax = plt.subplots(figsize=[10, 7])
-x = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-x *= (8 / 4096)
-errs = np.sqrt(math.sqrt(n_up ^ 2 + n_down ^ 2))
-
-ax.errorbar(x, the_thing_to_plot, errs, fmt='o', markersize=3.0, capsize=1.0, label='Data');
+ax.errorbar(bin_centers, up_minus_down, errs, fmt='o', markersize=3.0, capsize=1.0, label='Data')
 ax.set_title('Up counts - down counts vs time')
 ax.set_xlabel('Time ($\mu$s)')
-ax.set_ylabel('Up counts - down counts');
+ax.set_ylabel('Up counts - down counts')
+ax.plot(bin_centers, damped_sine(bin_centers, *popt), 'r-', label='fit')
+
 plt.show()
